9b398ff4894261f9772ba0b833e0fc7373b77893
nakochan <eunnamu094@gmail.com>
2020-07-22 15:23:28 +0900

    update

--- worker/app.js
+++ worker/app.js
@@ -2,7 +2,7 @@ const schedule = require('node-schedule')
 
 const reserved = ({ subject, type, label, board, extendedLink, maxPage, timeout, timer }) => {
     console.log(`[예약] ${subject} - ${timer}`)
-    // schedule.scheduleJob(timer, () => {
+    schedule.scheduleJob(timer, () => {
         const cron = require('./src/crawler/dcinside')
         cron({
             type,
@@ -12,18 +12,18 @@ const reserved = ({ subject, type, label, board, extendedLink, maxPage, timeout,
             maxPage,
             timeout
         })
-    // })
+    })
 }
 
-// reserved({
-//     subject: '디시인사이드 - HIT 갤러리',
-//     type: '디시',
-//     label: '힛갤',
-//     board: 'hit',
-//     maxPage: 3,
-//     timeout: 15000,
-//     timer: '00 00 * * * *'
-// })
+reserved({
+    subject: '디시인사이드 - HIT 갤러리',
+    type: '디시',
+    label: '힛갤',
+    board: 'hit',
+    maxPage: 3,
+    timeout: 15000,
+    timer: '00 00 * * * *'
+})
 
 reserved({
     subject: '디시인사이드 - 퍼온유머 갤러리',
