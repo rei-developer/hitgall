bcf987bd580bbe397b9453666b83b22310088f62
nakochan <eunnamu094@gmail.com>
2020-07-27 19:24:23 +0900

    update

--- service/backend/database/index.js
+++ service/backend/database/index.js
@@ -7,12 +7,12 @@ dotenv.config()
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env
 
 const pool = mysql.createPool({
-    connectionLimit: 8000,
+    connectionLimit: 4000,
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
     database: DB_DATABASE,
-    multipleStatements: true
+    // multipleStatements: true
 })
 
 pool.getConnection((err, connection) => {
