cda107f118a299d1655455a2147800f741b55107
nakochan <eunnamu094@gmail.com>
2020-07-27 16:45:50 +0900

    update

--- service/backend/api/cloud/controller.js
+++ service/backend/api/cloud/controller.js
@@ -22,46 +22,50 @@ const fs = require('fs')
 const sharp = require('sharp')
 
 module.exports.createImage = type => async ctx => {
-    const checker = /(.gif|.png|.jpg|.jpeg|.webp)/i.test(ctx.req.file.originalname)
-    const checkerByGIF = /.gif/i.test(ctx.req.file.originalname)
-    const filename = ctx.req.file.filename
-    // ex).webp.mp4 형식 수정
-    try {
-        if (checker) {
-            fs.readFile(`img/${filename}`, async (err, data) => {
-                if (err)
-                    return ctx.body = {
-                        message: err,
-                        status: 'fail'
-                    }
-                await uploadFile(`img/${filename}`)
-                if (type === 'topic') {
-                    const thumbnail = sharp(data)
-                    thumbnail
-                        .metadata()
-                        .then(() => thumbnail.resize(100, 100).withMetadata().rotate().toBuffer())
-                        .then(result => fs.writeFile(`img/thumb/${filename}`, result, async () => {
-                            await uploadFile(`img/thumb/${filename}`)
-                        }))
-                }
-            })
+    // const checker = /(.gif|.png|.jpg|.jpeg|.webp)/i.test(ctx.req.file.originalname)
+    // const checkerByGIF = /.gif/i.test(ctx.req.file.originalname)
+    // const filename = ctx.req.file.filename
+    // // ex).webp.mp4 형식 수정
+    // try {
+    //     if (checker) {
+    //         fs.readFile(`img/${filename}`, async (err, data) => {
+    //             if (err)
+    //                 return ctx.body = {
+    //                     message: err,
+    //                     status: 'fail'
+    //                 }
+    //             await uploadFile(`img/${filename}`)
+    //             if (type === 'topic') {
+    //                 const thumbnail = sharp(data)
+    //                 thumbnail
+    //                     .metadata()
+    //                     .then(() => thumbnail.resize(100, 100).withMetadata().rotate().toBuffer())
+    //                     .then(result => fs.writeFile(`img/thumb/${filename}`, result, async () => {
+    //                         await uploadFile(`img/thumb/${filename}`)
+    //                     }))
+    //             }
+    //         })
+    //         ctx.body = {
+    //             filename,
+    //             status: 'ok'
+    //         }
+    //     } else {
+    //         fs.unlink(`img/${filename}`, () => console.log(`삭제 : img/${filename}`))
+    //         ctx.body = {
+    //             message: 'gif, png, jpg, jpeg, webp만 가능',
+    //             status: 'fail'
+    //         }
+    //     }
+    // } catch (e) {
+    //     ctx.body = {
+    //         message: e.message,
+    //         status: 'fail'
+    //     }
+    // }
             ctx.body = {
-                filename,
-                status: 'ok'
-            }
-        } else {
-            fs.unlink(`img/${filename}`, () => console.log(`삭제 : img/${filename}`))
-            ctx.body = {
-                message: 'gif, png, jpg, jpeg, webp만 가능',
-                status: 'fail'
-            }
-        }
-    } catch (e) {
-        ctx.body = {
-            message: e.message,
+            message: '현재 DDoS 공격으로 잠시 중단함.',
             status: 'fail'
         }
-    }
 }
 
 /*
