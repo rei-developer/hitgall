caf494e902a0a6db5c74525669a575295cbe2dad
nakochan <eunnamu094@gmail.com>
2021-04-04 21:27:07 +0900

    서비스 워커 수정

--- service/plugins/service-worker.js
+++ service/plugins/service-worker.js
@@ -1,14 +1,12 @@
-if(process.client) {
-   const cacheName = `${process.env.packageVersionNumber}`
-    caches.keys().then((keyList) => {
-      return Promise.all(
-       keyList.map((key) => {
-       if (key !== cacheName) {
+if (process.client) {
+  const cacheName = `${process.env.packageVersionNumber}`
+  caches.keys().then((keyList) => {
+    return Promise.all(
+      keyList.map((key) => {
+        if (key !== cacheName) {
           return caches.delete(key)
-          }
-        })
-      )
-    })
-  }
-
- 
+        }
+      })
+    )
+  })
+}
